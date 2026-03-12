99cde05f36926810b36196076c6017ff6e6ffd67
vjrasane <3148501+vjrasane@users.noreply.github.com>
2026-03-12 16:35:39 +0200

    Add nested run() subcontext support in test script

--- test.ts
+++ test.ts
@@ -5,7 +5,7 @@ import {
   getInventoryHosts,
   define,
 } from "./packages/core/src/index.ts";
-import { context } from "./packages/core/src/run.ts";
+import { context, run } from "./packages/core/src/run.ts";
 
 const inventory = getInventorySync(
   path.join(import.meta.dirname, "inventory.yml"),
@@ -24,7 +24,10 @@ const main = async () => {
     hosts.map((h) =>
       h.run(
         define`Running on ${h.name}`(async () => {
-          const msg = await play("Hello " + h.name);
+          const msg = run`Subcontext`(async () => {
+            const msg = await play("Hello " + h.name);
+            return msg;
+          });
           context.log(`Got message: ${msg}`);
           await debug`Send message`({ msg: "Hello again!" });
           context.log(`Finished running on ${h.name}`);
